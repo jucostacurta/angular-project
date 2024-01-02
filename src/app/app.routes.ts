32fbf52d799ea6bfa5b3f44c77d82f74d42217b3
Juliana Costacurta <juliana.costacurta@consultores.rnp.br>
2024-01-02 16:02:36 -0300

    feat: adding first components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { CountryComponent } from './country/country.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Angular project',
+    component: HomeComponent,
+  },
+  {
+    path: 'country',
+    title: 'País',
+    component: CountryComponent,
+  },
+];
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CardComponent } from '../card/card.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CardComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {}
